44dd1caeb8d9c758a039f814da5505630e99b29a
Strekozov.Vladimir <strekozov_VA@hotmail.com>
2024-03-01 10:40:01 +0300

    terraform-hotfix-commit

--- 05/src/vpc/main.tf
+++ 05/src/vpc/main.tf
@@ -1,7 +1,12 @@
 terraform {
   required_providers {
     yandex = {
+      version = "0.109.0"
       source = "yandex-cloud/yandex"
+    }
+    template = {
+      version = "2.2.0"
+      source = "hashicorp/template"
     }
   }
   required_version = ">= 0.13"
